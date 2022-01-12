b1cf4a1ac4d3a0a875ccd9303dc8652a2072e8a8
Williams25 <william.souza.timao@hotmail.com>
2022-01-12 12:00:29 -0300

    Tornando o header dinâmico

--- src/components/SectionHero/index.tsx
+++ src/components/SectionHero/index.tsx
@@ -3,47 +3,45 @@ import React from "react";
 import Logo from "components/Logo";
 import Button from "components/Button";
 import * as S from "./styles";
-
+import { useImage } from "hooks/useImage";
 import { gaEvent } from "utils/ga";
 import Container from "components/Container";
-import { LogoProps } from "types/api";
+import { LogoProps, HeaderProps } from "types/api";
 
 const onClick = () =>
   gaEvent({ action: "click", category: "cta", label: "hero button" });
 
 type SectionHeroProps = {
   logo: LogoProps;
+  header: HeaderProps;
 };
 
-const SectionHero = ({ logo }: SectionHeroProps) => (
-  <S.Wrapper>
-    <Container>
-      <Logo alternativeText={logo.alternativeText} url={logo.url} />
+const SectionHero = ({ logo, header }: SectionHeroProps) => {
+  const { handleLoadImage } = useImage();
+  return (
+    <S.Wrapper>
+      <Container>
+        <Logo alternativeText={logo.alternativeText} url={logo.url} />
 
-      <S.Content>
-        <S.TextBlock>
-          <S.Title>React Avançado</S.Title>
-          <S.Description>
-            Crie aplicações reais com NextJS, Strapi, GraphQL e mais!
-          </S.Description>
-          <S.ButtonWrapper>
-            <Button
-              href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOJAN22"
-              onClick={onClick}
-              wide
-            >
-              Comprar
-            </Button>
-          </S.ButtonWrapper>
-        </S.TextBlock>
+        <S.Content>
+          <S.TextBlock>
+            <S.Title>{header.title}</S.Title>
+            <S.Description>{header.description}</S.Description>
+            <S.ButtonWrapper>
+              <Button href={header.button.url} onClick={onClick} wide>
+                {header.button.label}
+              </Button>
+            </S.ButtonWrapper>
+          </S.TextBlock>
 
-        <S.Image
-          src="/img/hero-illustration.svg"
-          alt="Ilustração de um desenvolvedor em frente a um computador com várias linhas de código."
-        />
-      </S.Content>
-    </Container>
-  </S.Wrapper>
-);
+          <S.Image
+            src={handleLoadImage(header.image.url)}
+            alt={header.image.alternativeText}
+          />
+        </S.Content>
+      </Container>
+    </S.Wrapper>
+  );
+};
 
 export default SectionHero;

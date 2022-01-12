9aff5d603d5b4e55e1f18c9f744bb232d1c80299
Williams25 <william.souza.timao@hotmail.com>
2022-01-12 12:01:06 -0300

    criandi type para o header

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,21 @@
 export type LandingPageProps = {
   logo: LogoProps;
+  header: HeaderProps;
 };
 
 export type LogoProps = {
   alternativeText: string;
   url: string;
+};
+
+export type ButtonProps = {
+  label: string;
+  url: string;
+};
+
+export type HeaderProps = {
+  title: string;
+  description: string;
+  button: ButtonProps;
+  image: LogoProps;
 };

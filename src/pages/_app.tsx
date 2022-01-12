f95e3bef92b1103e546aeb88ebe098994a1d9859
Williams25 <william.souza.timao@hotmail.com>
2022-01-12 11:00:26 -0300

    ajustando formatação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Head from 'next/head'
-import { NextSeo } from 'next-seo'
-import { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
+import React from "react";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
 
-import theme from 'styles/theme'
-import GlobalStyle from 'styles/global.styles'
+import theme from "styles/theme";
+import GlobalStyle from "styles/global.styles";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -20,25 +20,25 @@ const App = ({ Component, pageProps }: AppProps) => {
         description="Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!"
         canonical="https://reactavancado.com.br/"
         openGraph={{
-          url: 'https://reactavancado.com.br/',
+          url: "https://reactavancado.com.br/",
           title:
-            'React Avançado - Crie aplicações reais com NextJS, GraphQL e mais.',
+            "React Avançado - Crie aplicações reais com NextJS, GraphQL e mais.",
           description:
-            'Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!',
-          images: [{ url: 'https://reactavancado.com.br/img/cover.png' }],
-          site_name: 'React Avançado',
-          locale: 'pt_BR'
+            "Aprenda a criar um Ecommerce completo, indo do Backend ao Frontend utilizando tecnologias como ReactJS, NextJS, Strapi, GraphQL, Apollo e muito mais!",
+          images: [{ url: "https://reactavancado.com.br/img/cover.png" }],
+          site_name: "React Avançado",
+          locale: "pt_BR"
         }}
         twitter={{
-          handle: '@Willian_justen',
-          site: '@site',
-          cardType: 'summary_large_image'
+          handle: "@Willian_justen",
+          site: "@site",
+          cardType: "summary_large_image"
         }}
       />
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;

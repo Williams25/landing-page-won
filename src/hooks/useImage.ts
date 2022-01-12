71063df0383ede89806a4450d1a54dbe9330909b
Williams25 <william.souza.timao@hotmail.com>
2022-01-12 11:42:30 -0300

    utilizando Criando de ambiente

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,4 +1,4 @@
-export const useImage = (path = "http://localhost:1337") => {
+export const useImage = (path = process.env.NEXT_PUBLIC_IMAGE_HOST) => {
   const handleLoadImage = (url: string): string => {
     return `${path}${url}`;
   };

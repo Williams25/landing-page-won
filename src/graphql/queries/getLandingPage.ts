6c81cd66c03c51fa6d712dbc0b7c9099b3a5a572
Williams25 <william.souza.timao@hotmail.com>
2022-01-12 12:00:57 -0300

    criando fragment

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,37 @@
 export const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment image on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment button on ComponentPageButton {
+    label
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...image
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        ...button
+      }
+      image {
+        ...image
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
-      logo {
-        alternativeText
-        url
-      }
+      ...logo
+      ...header
     }
   }
 `;

78be8a15e5816929ae96f8409dd484338f5098c0
Williams25 <william.souza.timao@hotmail.com>
2022-01-17 09:20:28 -0300

    ajustando component pricingBox para receber props

--- src/components/PricingBox/index.tsx
+++ src/components/PricingBox/index.tsx
@@ -4,50 +4,45 @@ import Button from "components/Button";
 import { gaEvent } from "utils/ga";
 
 import * as S from "./styles";
+import { PricingBoxProps } from "types/api";
 
 const onClick = () =>
   gaEvent({ action: "click", category: "buy", label: "pricing box button" });
 
-const PricingBox = () => (
-  <S.Box>
-    <S.Prices>
-      <S.FullPrice>
-        De <span>R$549</span> por apenas
-      </S.FullPrice>
-      <S.DiscountPrice>
-        <span>6x de</span> R$66
-      </S.DiscountPrice>
-    </S.Prices>
-    <S.BenefitsList>
-      <S.BenefitsItem>
-        Acesso aos <strong>6 módulos</strong>
-      </S.BenefitsItem>
+const PricingBox = ({
+  button,
+  numberInstallments,
+  priceInstallment,
+  totalPrice,
+  benefits
+}: PricingBoxProps) => {
+  return (
+    <S.Box>
+      <S.Prices>
+        <S.FullPrice>
+          De <span>R${totalPrice}</span> por apenas
+        </S.FullPrice>
+        <S.DiscountPrice>
+          <span>{numberInstallments}x de</span> R${priceInstallment}
+        </S.DiscountPrice>
+      </S.Prices>
+      <S.BenefitsList>
+        {benefits.split("\n").map((item, index) => (
+          <S.BenefitsItem key={index}>{item}</S.BenefitsItem>
+        ))}
+      </S.BenefitsList>
 
-      <S.BenefitsItem>
-        Código de <strong>todo o projeto</strong> separado em commits
-      </S.BenefitsItem>
-
-      <S.BenefitsItem>
-        Contato <strong>direto</strong> com os instrutores via Slack
-      </S.BenefitsItem>
-
-      <S.BenefitsItem>
-        <strong>Lives exclusivas</strong> durante o curso
-      </S.BenefitsItem>
-    </S.BenefitsList>
-
-    <Button
-      href="https://www.udemy.com/course/react-avancado/?couponCode=PROMOJAN22"
-      onClick={onClick}
-      withPrice
-    >
-      <p>Comprar o curso</p>
-      <div>
-        <S.ButtonFullPrice>R$549</S.ButtonFullPrice>
-        <S.ButtonDiscountPrice>R$399</S.ButtonDiscountPrice>
-      </div>
-    </Button>
-  </S.Box>
-);
+      <Button href={button.url} onClick={onClick} withPrice>
+        <p>{button.label}</p>
+        <div>
+          <S.ButtonFullPrice>R${totalPrice}</S.ButtonFullPrice>
+          <S.ButtonDiscountPrice>
+            R${numberInstallments * priceInstallment}
+          </S.ButtonDiscountPrice>
+        </div>
+      </Button>
+    </S.Box>
+  );
+};
 
 export default PricingBox;
